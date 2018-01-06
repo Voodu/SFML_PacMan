fed857fce48b9f1afbfa0d461575e976cb8402ca
Piotr Ładoński <piolado@gmail.com>
2018-01-06 01:27:55 +0100

    Removed unnecessary code. Added onCollision temporary implementation

--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -1,40 +1,18 @@
 #include "../include/Fruit.hpp"
 
-Fruit::Fruit(sf::Color color, Transform transform) : color(color)
+Fruit::Fruit(sf::Color color, Transform transform) : StaticObject(color, transform)
 {
-    physical = false;
-    this->transform = transform;
     tag = "Fruit";
-    shape = sf::RectangleShape(sf::Vector2f(transform.rect.width, transform.rect.height));
-    shape.setPosition(transform.rect.left, transform.rect.top);
-    shape.setFillColor(color);
-}
-
-Fruit::Fruit(sf::Color color, float size) : Fruit(color, Transform(0, 0, size, size))
-{
 }
 
 Fruit::Fruit(Transform transform) : Fruit(sf::Color::Red, transform)
 {
 }
 
-void Fruit::init()
-{
-}
-
-void Fruit::update()
-{
-}
-
-void Fruit::render()
-{
-    scene->draw(shape);
-}
-
 void Fruit::onCollision(GameObject *other)
 {
-}
-
-void Fruit::parseMessage(std::string message)
-{
+    if (other->tag == "PacMan")
+    {
+        scene->removeGameObject(this);
+    }
 }
