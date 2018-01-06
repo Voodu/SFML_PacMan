73937abcda386ee9a520afc93783ad0c1748d87f
Piotr Ładoński <piolado@gmail.com>
2018-01-06 01:26:38 +0100

    Code cleanup

--- src/StaticObject.cpp
+++ src/StaticObject.cpp
@@ -1,10 +1,8 @@
 #include "../include/StaticObject.hpp"
 
-StaticObject::StaticObject(sf::Color color, Transform transform) : color(color)
+StaticObject::StaticObject(sf::Color color, Transform transform) : GameObject(transform), color(color)
 {
     physical = true;
-    this->transform = transform;
-    tag = "StaticObject";
     shape = sf::RectangleShape(sf::Vector2f(transform.rect.width, transform.rect.height));
     shape.setPosition(transform.rect.left, transform.rect.top);
     shape.setFillColor(color);
