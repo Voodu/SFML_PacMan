7f28f39f15b37aec6abe47cdc32cc93577c5a272
Piotr Ładoński <piolado@gmail.com>
2018-01-12 01:14:15 +0100

    Changed starting scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "../include/Engine.hpp"
-#include "../include/GameScene.hpp"
+#include "../include/MenuScene.hpp"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(560, 620), "PacMan");
-    Engine engine(window, new GameScene());
+    Engine engine(window, new MenuScene());
     engine.run();
 
     return 0;
